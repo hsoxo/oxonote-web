02babd0e951809165dcbfd57b3b7dfbd4a7dd7d8
Hs <ihebolong@gmail.com>
2020-05-18 10:02:39 +0800

    auto logout when get 401 response code

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { getToken } from '@/utils/auth'
+import { history } from '@/store'
 
 // create an axios instance
 const service = axios.create({
@@ -33,6 +34,10 @@ service.interceptors.response.use(
     // if the custom code is not 20000, it is judged as an error.
     if (response.status === 200 && response.data.code === 20000) {
       return response.data
+    } else if (response.status === 401) {
+      console.error(response.request.url)
+      history.push('/login')
+      return
     } else {
       return Promise.reject(new Error(response.data.code || 'Error'))
     }
